302ae6550a47255a97504ad31e6f7db95d7af98d
Omniasafwatt <me@example.com>
2023-09-08 18:59:45 -0700

    finish 2

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,18 +3,20 @@
 /**
  * main - entry point
  *
- * description: print all single digit number
+ * description: printnumbers of base16 in lowercase
  *
  * Return: always 0 (success)
 */
 
 int main(void)
 {
-	int digit = 48;
+	int digit = 48; /*48; decimal rep of 0*/
 
-	while (digit <= 102)
+	while (digit <= 102) /*102; decimal rep of f*/
 	{
 		putchar(digit);
+
+		/* after 9 we jump till 96; */
 		if (digit == 57)
 			digit += 39;
 		digit++;
