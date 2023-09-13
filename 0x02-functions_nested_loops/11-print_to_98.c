7ef3623a330578648aae02452bb9b91f902b9f78
Omniasafwatt <me@example.com>
2023-09-12 17:39:54 -0700

    finish

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function print
+ *
+ * @n: takes integer type input for function.
+*/
+
+void print_to_98(int n)
+{
+	int count;
+
+	if (n >98)
+		for (count = n; count >98; count--)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; count++)
+			printf("%d, ", count);
+	printf("98\n");
+}
