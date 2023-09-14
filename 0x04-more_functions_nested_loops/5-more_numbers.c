2768a3940354f7cfdb360be56017aa2531e3c170
Omniasafwatt <me@example.com>
2023-09-14 15:59:53 -0700

    finish

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14 10 times
+ *
+ * Return: always 0 (success)
+*/
+
+void more_numbers(void)
+{
+	int num = 0;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (num >= 0 && num <= 9)
+		{
+			_putchar(num + 48);
+			num++;
+		}
+		else if (num > 9 && num <= 14)
+		{
+			_putchar(1 + 48);
+			num = num % 10;
+			num++;
+		}
+	}
+	_putchar('\n');
+}
