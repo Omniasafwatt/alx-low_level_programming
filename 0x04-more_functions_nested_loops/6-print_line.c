3fd57b3074b802cce51cae6e323224986a015c33
Omniasafwatt <me@example.com>
2023-09-14 16:21:41 -0700

    finish

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+i#include "main.h"
+
+/**
+ * print_line - print 0 - 9
+ *
+ * @n: input charachter
+*/
+
+void print_line(int n)
+{
+	int num;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (num = 1; num <= n; num++)
+			_putchar('_');
+		_putchar('\n');
+
+	}
+}
