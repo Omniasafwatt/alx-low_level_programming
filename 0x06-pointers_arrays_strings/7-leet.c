f12bb85d7fb282f0993b161defce8181f412eac3
Omniasafwatt <me@example.com>
2023-09-21 13:29:51 -0700

    finish

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,7 +17,7 @@ char *leet(char *c)
 
 	while (*c)
 	{
-		for (i = 0; i < sizeof(key) / sizepf(char); i++)
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
 			if (*c == key[i] || *c == key[i] + 32)
 			{
