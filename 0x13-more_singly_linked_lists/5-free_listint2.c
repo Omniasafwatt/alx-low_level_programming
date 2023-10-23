953eeddfb0c92d2e219e066107a06fe1df3f9395
Omniasafwatt <omnia@yahoo.com>
2023-10-23 12:16:13 -0700

    ok

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * free_listint2 -free a list
+ * @head: addresses
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *temp;
+
+	if (!head)
+		return;
+
+	node = *head;
+	while (node)
+	{
+		temp = node;
+		node = node->next;
+		free(temp);
+	}
+	*head = NULL;
+}
