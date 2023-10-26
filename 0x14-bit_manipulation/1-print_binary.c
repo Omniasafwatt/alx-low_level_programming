a2e5ec9c1f6eea375c921db4babdc30a953b8a0b
Omniasafwatt <omnia@yahoo.com>
2023-10-26 09:33:16 -0700

    k

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,18 +7,18 @@
  */
 void print_binary(unsigned long int n)
 {       
-        int bit = sizeof(n) * 8, printed = 0;
+	int bit = sizeof(n) * 8, printed = 0;
 
-        while (bit)
-        {
-                if (n & 1l << --bit)
-                {
-                        _putchar('1');
-                        printed++;
-                }
-                else if (printed)
-                        _putchar('0');
-        }
-        if (!printed)
-                _putchar('0');
+	while (bit)
+	{
+		if (n & 1l << --bit)
+		{
+			_putchar('1');
+			printed++;
+		}
+		else if (printed)
+			_putchar('0');
+	}
+	if (!printed)
+		_putchar('0');
 }
