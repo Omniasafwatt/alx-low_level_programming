42d0acfb666586c788491c5897542e67452a657b
Omniasafwatt <omnia@yahoo.com>
2023-10-26 09:26:36 -0700

    ok

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the bit
+ * @n: the numbers to index
+ * @index: the bit to set
+ * Return: the bit state or -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+        if (index >= sizeof(n) * 8)
+                return (-1);
+        return (!!(*n |= 1L << index));
+}
